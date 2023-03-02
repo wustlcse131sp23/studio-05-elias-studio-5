8e77f9bbba0bbe238695452792d5c2bfa26e320d
EliasBarro <barro@wustl.edu>
2023-03-02 12:01:59 -0600

    drawBullsEye

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -35,17 +35,20 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0, 109,219);
+		StdDraw.filledCircle(x, y, (3.0/4.0)*radius);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146,0,0);
+		StdDraw.filledCircle(x, y, (1.0/2.0)*radius);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255,255,109);
+		StdDraw.filledCircle(x, y, (1.0/4.0)*radius);
 		
 	}
 
